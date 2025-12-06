aac63be12782b7ef0d717a4b22c6baf7e3de6838
bigmistqke <vandijckv@gmail.com>
2025-12-06 17:54:57 +0100

    feat: add vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import devtools from 'solid-devtools/vite'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   base: './',
-  plugins: [devtools(), solidPlugin()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        display: 'standalone',
+        icons: [
+          {
+            src: '/assets/cirkel.svg',
+            sizes: '48x48 72x72 96x96 128x128 256x256',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }) as unknown as Plugin,
+    devtools(),
+    solidPlugin(),
+  ],
   server: {
     port: 3000,
   },
